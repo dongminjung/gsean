4253232caf8704d80f34f7a87633c6d9238a9134
dongminjung <dmdmjung@gmail.com>
2018-01-24 21:45:35 +0900

    Update

--- R/gsean.R
+++ R/gsean.R
@@ -7,20 +7,20 @@ gsean <- function(geneset, x, exprs, pseudo = 1, threshold = 0.99, nperm = 1000,
   if(!is.character(x) & !is.numeric(x))
     stop("list or statistic is not appropriate for ORA or GSEA")
   
-  message("construct adjacency matrix ... ")
+  message("construct adjacency matrix ...... ")
   adjacency <- do.call("exprs2adj", c(list(exprs, pseudo = pseudo), corParam))
   if(is.numeric(x))
   {
-    message("GSEA ... ")
-    centrality.gsea(geneset, x, adjacency, pseudo = pseudo, centrality = centrality,
+    message("GSEA ...... ")
+    centrality_gsea(geneset, x, adjacency, pseudo = pseudo, centrality = centrality,
                     weightParam = weightParam, nperm = nperm, minSize = minSize,
                     maxSize = maxSize, gseaParam = gseaParam, nproc = nproc,
                     BPPARAM = BPPARAM)
   }
   else
   {
-    message("ORA ... ")
-    label.prop.gsea(geneset, x, adjacency, threshold = threshold, nperm = nperm,
+    message("ORA ...... ")
+    label_prop_gsea(geneset, x, adjacency, threshold = threshold, nperm = nperm,
                     minSize = minSize, maxSize = maxSize, gseaParam = gseaParam,
                     nproc = nproc, BPPARAM = BPPARAM, tmax = tmax, ...)
   }
